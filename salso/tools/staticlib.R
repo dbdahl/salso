92658d0bc65b591798a32e8231ba20041edf1196
David B. Dahl <dahl@stat.byu.edu>
2019-08-30 14:51:40 -0600

    Better static library.

--- salso/tools/staticlib.R
+++ salso/tools/staticlib.R
@@ -1,8 +1,10 @@
-#if ( FALSE && ( Sys.which("cargo") != "" ) ) {
-if ( Sys.which("cargo") != "" ) {
+if ( FALSE && ( Sys.which("cargo") != "" ) ) {
+#if ( Sys.which("cargo") != "" ) {
   system2("cargo",c("build","--release","--manifest-path=rustlib/Cargo.toml"))
   quit(status=0)
 }
+
+cat("Downloading static library.\n")
 
 osType <- function() {
   if ( .Platform$OS.type == "windows" ) "windows"
@@ -19,17 +21,7 @@ desc <- read.dcf("../DESCRIPTION")
 pkgName    <- as.character(desc[,"Package"])
 pkgVersion <- as.character(desc[,"Version"])
 
-print(sprintf("%s %s %s\n",pkgName,pkgVersion,osType))
-
-cat("I need to download.\n")
-
-
-# Build against depending static libs
-if ( FALSE ) {
-#if ( ! file.exists("../windows/gifski-0.8.6/include/gifski.h") ) {
-    download.file("https://github.com/rwinlib/gifski/archive/v0.8.6.zip", "lib.zip", quiet = TRUE)
-    dir.create("../windows", showWarnings = FALSE)
-    unzip("lib.zip", exdir = "../windows")
-    unlink("lib.zip")
-}
+download.file(sprintf("https://dbdahl.github.io/rpackages/lib/%s/%s/%s.tar.gz",osType,pkgName,pkgVersion), "staticlib.tar.gz", quiet=TRUE)
+untar(sprintf("staticlib.tar.gz",pkgVersion), exdir="..")
+unlink("staticlib.tar.gz")
 
