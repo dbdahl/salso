7c73bcf110fba85de8e11e1b840c0671d06753fe
David B. Dahl <dahl@stat.byu.edu>
2019-08-30 15:47:51 -0600

    Better build

--- salso/tools/staticlib.R
+++ salso/tools/staticlib.R
@@ -1,21 +1,30 @@
-if ( FALSE && ( Sys.which("cargo") != "" ) ) {
-#if ( Sys.which("cargo") != "" ) {
-  system2("cargo",c("build","--release","--manifest-path=rustlib/Cargo.toml"))
-  quit(status=0)
-}
+FORCE_DOWNLOAD <- FALSE
 
-cat("Downloading static library.\n")
+if ( ( ! FORCE_DOWNLOAD ) && ( Sys.which("cargo") != "" ) ) {
+  cat("\nCompiling static library.\n\n")
+  args <- commandArgs(TRUE)
+  target <- if ( length(args) > 0 ) paste0("--target",args[1]) else NULL
+  status <- system2("cargo",c("build",target,"--release","--manifest-path=rustlib/Cargo.toml"))
+  quit(status=status)
+}
 
 osType <- function() {
   if ( .Platform$OS.type == "windows" ) "windows"
   else {
     sysname <- Sys.info()["sysname"]
-    if ( sysname == "Darwin" ) "mac"
+    if ( sysname == "Darwin" ) "macosx"
     else if ( sysname == "Linux" ) "linux"
-    else ""
+    else sysname
   }
 }
 osType <- osType()
+
+if ( ! ( osType %in% c("windows","macosx","linux") ) ) {
+  cat(sprintf("\nThe package is not supported on this platform (%s).\n\n",osType))
+  quit(status=1)
+}
+
+cat("\nDownloading static library.\n\n")
 
 desc <- read.dcf("../DESCRIPTION")
 pkgName    <- as.character(desc[,"Package"])
