5958a86a27a1d8fbfc46b21b13df21817c91727a
David B. Dahl <dahl@stat.byu.edu>
2020-08-06 22:01:54 -0600

    Better summary

--- salso/R/plot.salso.summary.R
+++ salso/R/plot.salso.summary.R
@@ -1,22 +1,26 @@
 #' Heatmap, Dendrogram, and Exemplar Plotting for Partition Estimation
 #'
-#' This function produces one of three plots:  1. A heatmap showing the pairwise
-#' allocation probabilities that items are clustered. 2. A dendrogram based on
-#' expected partition loss showing the relationships among clusters when merging
-#' pairs of clusters such that the increase in the expectation of the posterior
-#' loss is minimized.  3. An exemplar plot showing pairs plots of all the
-#' variables with the exemplar (i.e., the most representative observation) of
-#' each cluster emphasized.
+#' This function produces one of four plots:  1. \code{"heatmap"}: A heatmap
+#' showing the pairwise allocation probabilities that items are clustered. 2.
+#' \code{"mds"}: A scatter plot using classical multidimensional scaling (also
+#' known as principal coordinates analysis) with the exemplar (i.e., the most
+#' representative observation) of each cluster emphasized.  3. \code{"pairs"}:
+#' Pairs plots of all the variables with the exemplar (i.e., the most
+#' representative observation) of each cluster emphasized. 4.
+#' \code{"dendrogram"}: A dendrogram based on expected partition loss showing
+#' the relationships among clusters when merging pairs of clusters such that the
+#' increase in the expectation of the posterior loss is minimized.
 #'
 #' @param x An object returned by \code{summary(x)}, where \code{x} itself is
 #'   returned by the \code{\link{salso}} function.
-#' @param type A string equal to \code{"heatmap"}, \code{"dendrogram"}, or
-#'   \code{"exemplar"}.
+#' @param type A string equal to \code{"heatmap"}, \code{"mds"}, \code{"pairs"},
+#'   or \code{"dendrogram"}.
 #' @param data The data from which the partition estimation was obtained.  This
-#'   is required when \code{type='exemplar'} and ignored otherwise.
+#'   is required when \code{type='pairs'} and ignored otherwise.
 #' @param showLabels Should the names of items be shown in the plot when
 #'   \code{type="heatmap"}?
-#' @param ... Currently ignored.
+#' @param ... Arguments to be passed to methods, such as graphical parameters
+#'   (see \code{\link{par}}).
 #'
 #' @return \code{NULL}, invisibly.
 #'
@@ -28,80 +32,96 @@
 #' est <- salso(draws, nCores=1)
 #' summ <- summary(est)
 #' plot(summ, type="heatmap")
+#' plot(summ, type="mds")
+#' plot(summ, type="pairs", data=iris)
 #' plot(summ, type="dendrogram")
-#' plot(summ, type="exemplar", data=iris)
 #'
-#' @seealso \code{\link{salso}}, \code{\link{summary.salso.estimate}}
+#' @seealso \code{\link{salso}}, \code{\link{summary.salso.estimate}},
+#'   \code{\link[stats]{cmdscale}}.
 #'
 #' @importFrom grDevices heat.colors rainbow topo.colors
 #' @importFrom graphics abline axis box image pairs par points polygon segments
 #'   text
 #' @export
 #'
-plot.salso.summary <- function(x, type=c("heatmap","dendrogram","exemplar")[1], data=NULL, showLabels=length(x$estimate)<=50, ...) {
+plot.salso.summary <- function(x, type=c("heatmap","mds","pairs","dendrogram")[1], data=NULL, showLabels=length(x$estimate)<=50, ...) {
+  opar <- NULL
   if ( type == "dendrogram" ) {
     plot(x$dendrogram)
-    return(invisible())
-  }
-  if ( type == "exemplar" ) {
-    if ( is.null(data) ) stop("'data' must be supplied when type='exemplar'")
-    m <- match(x$estimate, as.numeric(names(x$exemplar)))
-    i <- x$exemplar[m]
-    c <- rainbow(length(x$exemplar))[m]
-    panelFnc <- function(x0,y0,...) {
-      points(x0,y0,col=c,pch=19,...)
-      segments(x0,y0,x0[i],y0[i],col=c,...)
-      points(x0[x$exemplar],y0[x$exemplar],pch=22,bg="white",cex=2,...)
+  } else if ( type == "heatmap" ) {
+    estimate <- x$estimate
+    o <- x$order
+    pm <- x$psm
+    pm <- pm[o,rev(o)]
+    n <- nrow(pm)
+    sizes <- rle(estimate[o])$lengths
+    cuts <- cumsum(sizes)
+    centers <- ( c(0,cuts[-length(cuts)]) + cuts ) / 2
+    cuts <- cuts[-length(cuts)]
+    labels <- rle(estimate[o])$values
+    if ( showLabels ) {
+      mymar <- c(1.5,0.5,0.5,1.5)
+      cexscale <- 0.85 * 50 / length(estimate)
+    } else {
+      mymar <- c(0,0,0,0)
+      cexscale <- 1 * 50 / length(estimate)
     }
-    pairs(data,panel=panelFnc)
-    return(invisible())
-  }
-  if ( type != "heatmap" ) stop(sprintf("Unknown plot type '%s'.",type))
-  estimate <- x$estimate
-  o <- x$order
-  pm <- if ( ! is.null(attr(x$estimate,"psm")) ) attr(x$estimate,"psm") else psm(attr(x$estimate,"draws"))
-  pm <- pm[o,rev(o)]
-  n <- nrow(pm)
-  sizes <- rle(estimate[o])$lengths
-  cuts <- cumsum(sizes)
-  centers <- ( c(0,cuts[-length(cuts)]) + cuts ) / 2
-  cuts <- cuts[-length(cuts)]
-  labels <- rle(estimate[o])$values
-  if ( showLabels ) {
-    mymai <- c(1.5,0.5,0.5,1.5)
-    cexscale <- 0.85 * 50 / length(estimate)
+    opar <- par(pty="s",mar=mymar)
+    colors <- rev(heat.colors(200))
+    image(x=1:n,y=1:n,z=pm,axes=FALSE,xlab="",ylab="",col=colors)
+    box()
+    abline(v=cuts+0.5,lwd=3)
+    abline(h=n-cuts+0.5,lwd=3)
+    text(centers+0.5,n-centers+0.5,labels,cex=0.8*cexscale*sizes)
+    if ( showLabels ) {
+      axisLabels <- if ( is.null(names(estimate)) ) o
+      else names(estimate[o])
+      axis(4,1:length(estimate),rev(axisLabels),las=2,cex.axis=0.8*cexscale)
+      axis(1,1:length(estimate),axisLabels,las=2,cex.axis=0.8*cexscale)
+      nn <- length(colors)
+      bx <- par("usr")
+      bx.cx <- c(bx[1] - 1.6 * (bx[2] - bx[1]) / 50, bx[1] - 0.3 * (bx[2] - bx[1]) / 50)
+      bx.cy <- c(bx[3], bx[3])
+      bx.sy <- (bx[4] - bx[3]) / nn
+      xx <- rep(bx.cx, each=2)
+      for ( i in 1:nn ) {
+        yy <- c(bx.cy[1] + (bx.sy * (i - 1)),
+                bx.cy[1] + (bx.sy * (i)),
+                bx.cy[1] + (bx.sy * (i)),
+                bx.cy[1] + (bx.sy * (i - 1)))
+        polygon(xx,yy,col=colors[i],border=colors[i],xpd=TRUE)
+      }
+    }
   } else {
-    mymai <- c(0,0,0,0)
-    cexscale <- 1 * 50 / length(estimate)
+    colors <- rainbow(x$nClusters)
+    colorsExpanded <- colors[x$estimate]
+    i <- x$exemplar[x$estimate]
+    if ( type == "pairs" ) {
+      if ( is.null(data) ) stop("'data' must be supplied when type='exemplar'")
+      panelFnc <- function(x0,y0,...) {
+        points(x0,y0,col=colorsExpanded,pch=19,...)
+        segments(x0,y0,x0[i],y0[i],col=colorsExpanded,...)
+        points(x0[x$exemplar],y0[x$exemplar],pch=22,bg="white",cex=2,...)
+      }
+      opar <- par(no.readonly=TRUE)
+      pairs(data, panel=panelFnc, oma=c(3,10,3,3))
+      legend("topleft", legend=as.character(seq_len(x$nClusters)), pch=20, col=colors, xpd=TRUE, ...)
+    } else if ( type == "mds" ) {
+      distances <- 1 - x$psm
+      points <- cmdscale(distances, list.=TRUE)$points
+      # points <- MASS::isoMDS(distances)$points
+      opar <- par(pty="s", mar=c(0,0,0,0))
+      plot(points[,1], points[,2], pch=20, col=colorsExpanded, type=ifelse(showLabels, "n", "p"), xlab="", ylab="", axes=FALSE, ...)
+      box()
+      segments(points[,1],points[,2],points[i,1],points[i,2],col=colorsExpanded,...)
+      if ( showLabels ) {
+        text(points[,1], points[,2], as.character(seq_along(x$estimate)), col=colorsExpanded, ...)
+      }
+      points(points[x$exemplar,1],points[x$exemplar,2],pch=22,bg="white",cex=2,...)
+      legend("topleft", legend=as.character(seq_len(x$nClusters)), pch=20, col=colors, ...)
+    } else stop(sprintf("Unknown plot type '%s'.",type))
   }
-  opar <- par(pty="s",mai=mymai)
-  colors <- topo.colors(200)
-  colors <- rev(heat.colors(200))
-  image(x=1:n,y=1:n,z=pm,axes=FALSE,xlab="",ylab="",col=colors)
-  box()
-  abline(v=cuts+0.5,lwd=3)
-  abline(h=n-cuts+0.5,lwd=3)
-  text(centers+0.5,n-centers+0.5,labels,cex=0.8*cexscale*sizes)
-  if ( showLabels ) {
-    axisLabels <- if ( is.null(names(estimate)) ) o
-    else names(estimate[o])
-    axis(4,1:length(estimate),rev(axisLabels),las=2,cex.axis=0.8*cexscale)
-    axis(1,1:length(estimate),axisLabels,las=2,cex.axis=0.8*cexscale)
-    nn <- length(colors)
-    bx <- par("usr")
-    bx.cx <- c(bx[1] - 1.6 * (bx[2] - bx[1]) / 50, bx[1] - 0.3 * (bx[2] - bx[1]) / 50)
-    bx.cy <- c(bx[3], bx[3])
-    bx.sy <- (bx[4] - bx[3]) / nn
-    xx <- rep(bx.cx, each=2)
-    for ( i in 1:nn ) {
-      yy <- c(bx.cy[1] + (bx.sy * (i - 1)),
-              bx.cy[1] + (bx.sy * (i)),
-              bx.cy[1] + (bx.sy * (i)),
-              bx.cy[1] + (bx.sy * (i - 1)))
-      polygon(xx,yy,col=colors[i],border=colors[i],xpd=TRUE)
-    }
-  }
-  par(opar)
+  if ( ! is.null(opar) ) par(opar)
   invisible()
 }
 
--- salso/R/summary.salso.estimate.R
+++ salso/R/summary.salso.estimate.R
@@ -1,11 +1,20 @@
 #' Summary of Partitions Estimated Using Posterior Expected Loss
 #'
+#' Assessing the quality of clusters in a partition estimate is added by this
+#' function. The result can then be plotted with
+#' \code{\link{plot.salso.summary}}.  The current implementation of the
+#' calculation of these summaries is not terribly efficient and may be improved
+#' in the future.
+#'
 #' @param object An object returned by the \code{\link{salso}} function.
 #' @param ... Currently ignored
 #'
-#' @return A list containing the score for each observation, exemplar
-#'   observation for each cluster, a dendrogram object, and a vector for
-#'   ordering observations in the heatmap plot.
+#' @return A list containing the pairwise similarity matrix, the mean pairwise
+#'   similarity matrix, the score and mean pairwise similarity for each
+#'   observation, exemplar observation for each cluster, a dendrogram object, a
+#'   vector for ordering observations in the heatmap plot, the size of each
+#'   cluster, and the number of clusters.
+#'
 #' @export
 #'
 #' @examples
@@ -14,8 +23,9 @@
 #' est <- salso(draws, nCores=1)
 #' summ <- summary(est)
 #' plot(summ, type="heatmap")
+#' plot(summ, type="mds")
+#' plot(summ, type="pairs", data=iris)
 #' plot(summ, type="dendrogram")
-#' plot(summ, type="exemplar", data=iris)
 #'
 summary.salso.estimate <- function(object, ...) {
   loss <- attr(object,"info")$loss
@@ -23,13 +33,19 @@ summary.salso.estimate <- function(object, ...) {
   isPSM <- isPSM(x)
   nItems <- length(object)
   one2n <- seq_len(nItems)
+  # pairwise similarity matrix
+  psm <- if ( isPSM ) x else psm(x)
   # score
   score <- sapply(one2n, function(i) {
-    subset <- ( object==object[i] ) & ( one2n!= i )
+    subset <- ( object == object[i] ) & ( one2n != i )
     xi <- if ( isPSM ) x[subset, subset, drop=FALSE] else x[, subset, drop=FALSE]
     partition.loss(rep(1,ncol(xi)), xi, loss)
   })
   score[is.na(score)] <- 0
+  # meanPS
+  meanPS <- sapply(seq_along(object), function(i) {
+    mean(psm[object==object[i], i])
+  })
   # exemplar
   all <- data.frame(id=seq_len(nItems), label=as.vector(object), score=score)
   exemplar <- sapply(split(all, all$label), function(d) {
@@ -48,10 +64,24 @@ summary.salso.estimate <- function(object, ...) {
     order
   }
   order <- order(order(walk(dendrogram))[object], -1*score)
-  # matrix
-  matrix <- NULL
+  # nClusters
+  nClusters <- length(exemplar)
+  # pairwise similarity matrix averaged by clustering estimate
+  m <- matrix(0.0, nrow=nClusters, ncol=nClusters)
+  for ( i in seq_len(nClusters) ) {
+    for ( j in seq_len(i) ) {
+      m[i,j] <- mean(psm[object==i, object==j])
+    }
+  }
+  ut <- upper.tri(m)
+  m[ut] <- t(m)[ut]
+  # sizes
+  sizes <- table(object)
+  names <- names(sizes)
+  sizes <- as.vector(sizes)
+  names(sizes) <- names
   # Finalize
-  result <- list(estimate=object, score=score, dendrogram=dendrogram, order=order, matrix=matrix, exemplar=exemplar)
+  result <- list(estimate=object, nClusters=nClusters, score=score, order=order, dendrogram=dendrogram, psm=psm, meanPS=meanPS, meanPSM=m, exemplar=exemplar, sizes=sizes)
   class(result) <- "salso.summary"
   result
 }
