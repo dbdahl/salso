8a639374ef7f1b3597aea64fdbd0cff5c53ac0b9
David B. Dahl <dahl@stat.byu.edu>
2021-03-05 21:23:09 -0700

    Include ORCID

--- salso/tools/staticlib.R
+++ salso/tools/staticlib.R
@@ -1,11 +1,17 @@
+if ( ! cargo::is_available("1.40") ) {
+
+  cargo:::download_static_library(target, function(osName,pkgName,pkgVersion) {
+    sprintf("https://dbdahl.github.io/rpackages/lib/%s/%s/%s.tar.gz",osName,pkgName,pkgVersion)
+  })
+
+  quit(status=0)
+
+}
+
 args <- commandArgs(TRUE)
 target <- args[1]
 targetArg <- if ( is.na(target) ) NULL else paste0("--target=",args[1])
 
-status <- cargo::run(c("build", targetArg, "--release", "--manifest-path=rustlib/Cargo.toml"),
-                     minimum_version="1.31", verbose=TRUE)
+status <- cargo::run(c("build", targetArg, "--release", "--manifest-path=rustlib/Cargo.toml"))
 
-if ( ! is.null(status) && ( status == 0 ) ) quit(status=0)
-
-cargo:::download_static_library(target,
-        function(osName,pkgName,pkgVersion) sprintf("https://dbdahl.github.io/rpackages/lib/%s/%s/%s.tar.gz",osName,pkgName,pkgVersion))
+quit(status=status)
