e9a8ef031db9254fe5e457efc73568823e74fbef
David B. Dahl <dahl@stat.byu.edu>
2021-03-05 22:51:03 -0700

    Fixed downloading of static library

--- salso/tools/staticlib.R
+++ salso/tools/staticlib.R
@@ -1,17 +1,13 @@
-if ( ! cargo::is_available("1.40") ) {
+args <- commandArgs(TRUE)
+target <- args[1]
 
+if ( ! cargo::is_available("1.40") ) {
   cargo:::download_static_library(target, function(osName,pkgName,pkgVersion) {
     sprintf("https://dbdahl.github.io/rpackages/lib/%s/%s/%s.tar.gz",osName,pkgName,pkgVersion)
   })
-
   quit(status=0)
-
 }
 
-args <- commandArgs(TRUE)
-target <- args[1]
 targetArg <- if ( is.na(target) ) NULL else paste0("--target=",args[1])
-
 status <- cargo::run(c("build", targetArg, "--release", "--manifest-path=rustlib/Cargo.toml"))
-
 quit(status=status)
