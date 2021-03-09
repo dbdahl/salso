9e58ecacaae100624ac1e26c22808e0952877c97
David B. Dahl <dahl@stat.byu.edu>
2021-03-08 21:19:20 -0700

    Better build script

--- salso/tools/staticlib.R
+++ salso/tools/staticlib.R
@@ -1,16 +1,24 @@
-target <- commandArgs(TRUE)[1]
+statlib_template <- commandArgs(TRUE)[1]
+statlib          <- commandArgs(TRUE)[2]
+target <- cargo::target()
 
-if ( ! cargo::is_available("1.40") ) {
+cat("Target is: ", target, "\n", sep="")
+
+if ( cargo::is_available("1.40") ) {
+
+  cargo::run(c("build", paste0("--target=",target), "--release", "--manifest-path=rustlib/Cargo.toml"))
+
+} else {
+
   cargo:::download_static_library(target,
-    mkURL2=function(pkgName,pkgVersion,osName,target) {
+    mkURL1=function(pkgName,pkgVersion,osName,target) {
       sprintf("https://daviddahl.org/rrepository/staticlib/%s_%s/%s/%s.tar.gz",pkgName,pkgVersion,osName,target)
     },
-    mkURL1=function(pkgName,pkgVersion,osName,target) {
+    mkURL2=function(pkgName,pkgVersion,osName,target) {
       sprintf("https://dahl.byu.edu/rrepository/staticlib/%s_%s/%s/%s.tar.gz",pkgName,pkgVersion,osName,target)
     }
-    )
-  quit(status=0)
+  )
+
 }
 
-status <- cargo::run(c("build", paste0("--target=",target), "--release", "--manifest-path=rustlib/Cargo.toml"))
-quit(status=status)
+file.copy(file.path(gsub("___",target,statlib_template),basename(statlib)), statlib)
