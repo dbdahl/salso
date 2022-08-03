0c6c40a178fb50146086aeb956b5470d513ffefe
David B. Dahl <dahl@stat.byu.edu>
2022-08-02 21:38:16 -0600

    Getting ready to resubmit

--- salso/tools/registration.R
+++
@@ -1,8 +0,0 @@
-.Call <- base::.Call
-.bell <- NULL
-.enumerate_partitions <- NULL
-.expected_loss <- NULL
-.lbell <- NULL
-.minimize_by_enumeration <- NULL
-.psm <- NULL
-.minimize_by_salso <- NULL
