efd9d489c31f4614bc464a67a86c9ffb11d83f72
David B. Dahl <dahl@stat.byu.edu>
2026-04-11 11:26:12 -0600

    Preparing for another submission

--- salso/src/r_init_shim.c
+++ salso/src/r_init_shim.c
@@ -1,4 +1,4 @@
-// Automatically regenerated. Do not edit.
+// Forwards R's package init to the Rust-defined registration function.
 
 void R_init_salso_rust(void *dll); 
 void R_init_salso(void *dll) { R_init_salso_rust(dll); }
